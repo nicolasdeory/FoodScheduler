a242af9ce3eb69b30cded9293c567ba517c87662
Nicolás de Ory <deorynicolas@gmail.com>
2020-04-26 17:44:13 +0200

    Responsive schedule section. Adjusted alert box

--- js/schedule.js
+++ js/schedule.js
@@ -3,7 +3,7 @@ $(function()
 
     function setDayHeaderPositions() 
     {
-        var widthTableCell = $(`#schd-table td`).width();
+        var widthTableCell = $(`#schd-table td`).width() + 40; // padding
         for (let i = 0; i < 8; i++) 
         {
             let widthSpan = $(`#day-container span`).eq(i).width();
@@ -16,6 +16,37 @@ $(function()
     $(window).on('resize', function(e)
     {
         setDayHeaderPositions();
+    });
+
+
+    var isSyncingTopScroll = false;
+    var isSyncingDownScroll = false;
+
+    $("#day-container").scroll(function(e)
+    {
+        if (!isSyncingDownScroll)
+        {
+            isSyncingTopScroll = true;
+            if (!e.originalEvent) return;
+            //let scrollPct = $("#day-container").scrollLeft() / $("#day-container").width();
+            //$("#schd-table-cont").scrollLeft(scrollPct * ($("#schd-table-cont").width()))
+            $("#schd-table-cont").scrollLeft($("#day-container").scrollLeft());
+            //console.log("day" + scrollPct);
+        }
+        isSyncingDownScroll = false
+    });
+
+    $("#schd-table-cont").scroll(function(e)
+    {
+        if (!isSyncingTopScroll) {
+            isSyncingDownScroll = true;
+            if (!e.originalEvent) return;
+            //let scrollPct = $("#schd-table-cont").scrollLeft() / ($("#schd-table-cont").width()-70);
+            //console.log(scrollPct);
+            //$("#day-container").scrollLeft(scrollPct * ($("#day-container").width()-80))
+            $("#day-container").scrollLeft($("#schd-table-cont").scrollLeft());
+        }
+        isSyncingTopScroll = false;
     });
 
     setDayHeaderPositions();
