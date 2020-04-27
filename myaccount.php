fc76186a848a092fd07cc22193ae20225e1599d4
Badayco Rijo <badarhndz@gmail.com>
2020-04-27 14:43:44 +0100

    My Account design

--- myaccount.php
+++ myaccount.php
@@ -14,12 +14,41 @@
 
 <!--Link al archivo css-->   
     <link rel="stylesheet" href="assets/css/myaccountstyle.css" type="text/css">
+
+<link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
 
 <body>
-
-    <!--Párrafo-->
+<!--Párrafo-->
         <h2>Mi cuenta</h2>
+
+<!--Menú de navegación-->
+<div class="navegacion">
+    <div class="menu">
+        <div class="izquierda">
+          <div class="burger"> <span class="material-icons toggle">menu</span></div>
+          <div class= "titulo"> <p>Planificador Alimenticio</p> </div>
+        </div>
+        <div class="partederechamenu">
+            <span class="material-icons campanades">notifications</span>
+            <p> Hola, Nicolas </p>
+            <p> Mi cuenta </p>
+            <p> Salir </p>
+        </div>
+    </div>
+</div>
+
+
+<script>
+  let menu = document.querySelector('.toggle')
+  menu.addEventListener('click', (e) => {
+    document.querySelector('.partederechamenu').classList.toggle('active');
+    document.querySelector('.navegacion').classList.toggle('activenav');
+  });
+</script>
+          
 
 
 
