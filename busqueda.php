1706ccf466a7f6e19063067cf2e50a61dad99ac2
Fernando <frabascol@gmail.com>
2020-04-27 12:13:44 +0200

    Initial commit search

---
+++ busqueda.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="ES">
+
+
+<header>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BARRA SUPERIOR-->
+
+    <link rel="stylesheet" href="css/busqueda.css">
+</header>
+
+<body>
+
+    <div class="title">
+        <h1>Encontrar Recetas</h1>
+    </div>
+
+    <div class="line">
+        <hr>
+    </div>
+
+    <div class="subtitle">
+        <p1>Con estos parámetros, tienes estas recetas disponibles</p1>
+    </div>
+
+    <div class="contenedor">
+        <div class="search">
+            <div class="comida">
+                <input type="text" placeholder="Elige una comida" class="input-text"></input>
+            </div>
+
+            <div class="ingrediente">
+                Elige un ingrediente
+            </div>
+
+
+            <div class="dificultad">
+                
+                    <img class="img" src="icons/user-solid.svg">
+                    
+                    <div class="texto-dif">
+                        <p1>Nivel de dificultad</p1>
+                    </div>
+                
+
+            </div>
+
+            <div class="boton">
+                <h3>Buscar</h3>
+            </div>
+        </div>
+
+        <div class="result">
+
+        </div>
+
+        <div class="result">
+
+        </div>
+
+        <div class="result">
+
+        </div>
+
+
+    </div>
+
+
+
+</body>
+
+
+
+
+</html>
