59122eaa9e8091a60acc18e4194f9fa9f529da8f
Fernando <frabascol@gmail.com>
2020-05-07 00:44:11 +0200

    Added icons and info to the recipe

--- busqueda.php
+++ busqueda.php
@@ -114,11 +114,56 @@
                 </div>
 
                 <div class="result">
+                    <div class="photo">
+                        <img class="spaguetti" src="images/photo1.jpg">
+                    </div>
+                    <div class="description">
+                        <div class="recipetitle">
+                             Espaguetis a la boloñesa
+                        </div>
+                        <div class="info">
+                            <div class="like">
+                                <div class="likeicon"> <span class="material-icons iconocolumna"> favorite </span> </div>
+                                <div class="numberlikes"> 276</div>
+                            </div>
+                            <div class="time">
+                                <div class="timeicon"><i class="far fa-clock"></i></div>
+                                <div class="amounttime"> 20 min</div>
+                            </div>
+                            <div class="difficulty">
+                            <div class="texto-dif">
+                            <div class="dif">
+                                    <div class="rating">
+                                        
+                                            <div class="star"></div>                                  
+                                            <div class="star"></div>
+                                            <div class="star"></div>
+                                            <div class="star"></div>
+                                            <div class="star"></div>
+                                       
+                                        <div class="text" id="rating-label-text">Facil</div>
+                                        <div class="clear"></div>
+                                    </div>
+                            </div>
+                        </div>
 
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="result">
-
+                    <div class="photo">
+                    </div>
+                    <div class="description">
+                        <div class="recipetitle">
+                        </div>
+                        <div class="info">
+                            <div class="like"></div>
+                            <div class="time"></div>
+                            <div class="difficulty"></div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="result">
