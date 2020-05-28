3dd84359192b1da040b83d029c354a80b4faa70f
Fernando <frabascol@gmail.com>
2020-05-28 22:21:29 +0200

    Finished the createrecipe.css and new objects in createrecipe.php

--- createrecipe.php
+++ createrecipe.php
@@ -67,101 +67,238 @@ if (!isset($_SESSION['login'])) {
             </div>
 
             <div class="contenedor" id="contenedor">
-                <div class="nombre">
-                    <div class="texto-antes">
-                        <p>Elige un nombre para la receta</p>
-                    </div>
-                    <div class="comida">
-                        <div class="icon1">
-                            <i class="fas fa-utensils"></i>
+                <div class="arriba">
+                    <div class="nombre">
+                        <div class="texto-antes">
+                            <p>Elige un nombre para la receta</p>
                         </div>
-                        <input type="text" placeholder="" id="input-nombre" class="input-text"></input>
-                    </div>
-
-
-
-                    <div class="texto-antes">
-                        <p>Dificultad de la receta</p>
-                    </div>
-                    <div class="dificultad">
-                        <div class="icon3">
-                            <i class="fas fa-user"></i>
-                        </div>
-
-
-                        <div class="texto-dif">
-                            <div class="dif">
-                                <form>
-                                    <div class="rating">
-                                        <input id="star5" name="star" type="radio" value="5" class="radio-btn hide" />
-                                        <label for="star5">
-                                            <div class="star"></div>
-                                        </label>
-                                        <input id="star4" name="star" type="radio" value="4" class="radio-btn hide" />
-                                        <label for="star4">
-                                            <div class="star"></div>
-                                        </label>
-                                        <input id="star3" name="star" type="radio" value="3" class="radio-btn hide" />
-                                        <label for="star3">
-                                            <div class="star"></div>
-                                        </label>
-                                        <input id="star2" name="star" type="radio" value="2" class="radio-btn hide" />
-                                        <label for="star2">
-                                            <div class="star"></div>
-                                        </label>
-                                        <input id="star1" name="star" type="radio" value="1" class="radio-btn hide" />
-                                        <label for="star1">
-                                            <div class="star"></div>
-                                        </label>
-                                        <div class="text" id="rating-label-text">Dificultad</div>
-                                        <div class="clear"></div>
-                                    </div>
-                                </form>
+                        <div class="comida">
+                            <div class="icon1">
+                                <i class="fas fa-utensils"></i>
                             </div>
+                            <input type="text" placeholder="Nombre" id="input-nombre" class="input-text"></input>
                         </div>
 
 
-
-                    </div>
-                    <div class="publica">
-                        <p>¿Es pública la receta?</p>
-                    </div>
-                </div>
-                <div class="contenedoringredientes">
-                    <div class="ingredientenuevo">
 
                         <div class="texto-antes">
-                            <p>Introduce los ingredientes que componen la receta</p>
+                            <p>Dificultad de la receta</p>
                         </div>
-                        <div class="ingrediente">
-                            <div class="ing">
-                                <div class="icon2">
-                                    <i class="fas fa-cheese"></i>
-                                </div>
-                                <input type="text" placeholder="" id="input-ingrediente" class="input-ing"></input>
+                        <div class="dificultad">
+                            <div class="icon3">
+                                <i class="fas fa-user"></i>
                             </div>
-                            <input type="number" placeholder="Cantidad" id="input-ingrediente" class="input-ing"></input>
+                            <div class="texto-dif">
+                                <div class="dif">
+                                    <form>
+                                        <div class="rating">
+                                            <input id="star5" name="star" type="radio" value="5" class="radio-btn hide" />
+                                            <label for="star5">
+                                                <div class="star"></div>
+                                            </label>
+                                            <input id="star4" name="star" type="radio" value="4" class="radio-btn hide" />
+                                            <label for="star4">
+                                                <div class="star"></div>
+                                            </label>
+                                            <input id="star3" name="star" type="radio" value="3" class="radio-btn hide" />
+                                            <label for="star3">
+                                                <div class="star"></div>
+                                            </label>
+                                            <input id="star2" name="star" type="radio" value="2" class="radio-btn hide" />
+                                            <label for="star2">
+                                                <div class="star"></div>
+                                            </label>
+                                            <input id="star1" name="star" type="radio" value="1" class="radio-btn hide" />
+                                            <label for="star1">
+                                                <div class="star"></div>
+                                            </label>
+                                            <div class="text" id="rating-label-text">Dificultad</div>
+                                            <div class="clear"></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
 
-                            <select name="unidadDeMedida">
-                                <option value="Unidad">Unidad</option>
-                                <option value="Gramo">Gramos</option>
-                                <option value="Mililitro">Mililitro</option>
 
 
-                            </select>
+                        </div>
+                        <div class="texto-antes">
+                            <p>¿Es pública la receta?</p>
+                        </div>
+                        <div class="publica">
+                            <input class="input-pub" id="public" type="radio" value="1" placeholder="Pública" checked>
+                            <label class="input-pub" for="public">Pública</label><br>
+                            <input class="input-pub" id="private" type="radio" value="2" placeholder="Privada">
+                            <label class="input-pub" for="public">Privada</label><br>
+                        </div>
+                    </div>
+                    <div class="contenedoringredientes">
+                        <div class="ingredientenuevo">
 
+                            <div class="texto-antes">
+                                <p>Nuevo ingrediente </p>
+                            </div>
+                            <div class="ingrediente">
+                                <div class="ing">
+                                    <div class="icon2">
+                                        <i class="fas fa-cheese"></i>
+                                    </div>
+                                    <input type="text" placeholder="Nombre" id="input-ingrediente" class="input-ing"></input>
+                                </div>
+                                <div class="cant">
+                                    <input type="number" placeholder="Cantidad" id="input-ingrediente" class="input-ing"></input>
+
+                                </div>
+                                <div class="unid">
+
+
+                                    <select name="unidadDeMedida" class="unidadmed">
+                                        <option value="Unidad">Unidad</option>
+                                        <option value="Gramo">Gramos</option>
+                                        <option value="Mililitro">Mililitro</option>
+
+
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="ingredientenuevo">
+
+                            <div class="texto-antes">
+                                <p>Nuevo ingrediente</p>
+                            </div>
+                            <div class="ingrediente">
+                                <div class="ing">
+                                    <div class="icon2">
+                                        <i class="fas fa-cheese"></i>
+                                    </div>
+                                    <input type="text" placeholder="Nombre" id="input-ingrediente" class="input-ing"></input>
+                                </div>
+                                <div class="cant">
+                                    <input type="number" placeholder="Cantidad" id="input-ingrediente" class="input-ing"></input>
+
+                                </div>
+                                <div class="unid">
+
+
+                                    <select name="unidadDeMedida" class="unidadmed">
+                                        <option value="Unidad">Unidad</option>
+                                        <option value="Gramo">Gramos</option>
+                                        <option value="Mililitro">Mililitro</option>
+
+
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="ingredientenuevo">
+
+                            <div class="texto-antes">
+                                <p>Nuevo ingrediente</p>
+                            </div>
+                            <div class="ingrediente">
+                                <div class="ing">
+                                    <div class="icon2">
+                                        <i class="fas fa-cheese"></i>
+                                    </div>
+                                    <input type="text" placeholder="Nombre" id="input-ingrediente" class="input-ing"></input>
+                                </div>
+                                <div class="cant">
+                                    <input type="number" placeholder="Cantidad" id="input-ingrediente" class="input-ing"></input>
+
+                                </div>
+                                <div class="unid">
+
+
+                                    <select name="unidadDeMedida" class="unidadmed">
+                                        <option value="Unidad">Unidad</option>
+                                        <option value="Gramo">Gramos</option>
+                                        <option value="Mililitro">Mililitro</option>
+
+
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="ingredientenuevo">
+
+                            <div class="texto-antes">
+                                <p>Nuevo ingrediente</p>
+                            </div>
+                            <div class="ingrediente">
+                                <div class="ing">
+                                    <div class="icon2">
+                                        <i class="fas fa-cheese"></i>
+                                    </div>
+                                    <input type="text" placeholder="Nombre" id="input-ingrediente" class="input-ing"></input>
+                                </div>
+                                <div class="cant">
+                                    <input type="number" placeholder="Cantidad" id="input-ingrediente" class="input-ing"></input>
+
+                                </div>
+                                <div class="unid">
+
+
+                                    <select name="unidadDeMedida" class="unidadmed">
+                                        <option value="Unidad">Unidad</option>
+                                        <option value="Gramo">Gramos</option>
+                                        <option value="Mililitro">Mililitro</option>
+
+
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
+                <div class="abajo">
+                    <div class="boton">
+                        <button class="button" type="button" id="buscar">
+                            Añadir Receta
+                        </button>
+                    </div>
+
+
+                    <div class="contenedorpasos">
+                        <div class="paso">
+                            <div class="texto-antes">
+                                <p>Paso número: _</p>
+                            </div>
+                            <input class="input-paso" type="text" placeholder="Describe cómo realizar este paso">
+                        </div>
+
+                        <div class="paso">
+                            <div class="texto-antes">
+                                <p>Paso número: _</p>
+                            </div>
+                            <input class="input-paso" type="text" placeholder="Describe cómo realizar este paso">
+                        </div>
+
+                        <div class="paso">
+                            <div class="texto-antes">
+                                <p>Paso número: _</p>
+                            </div>
+                            <input class="input-paso" type="text" placeholder="Describe cómo realizar este paso">
+                        </div>
+
+                        <div class="paso">
+                            <div class="texto-antes">
+                                <p>Paso número: _</p>
+                            </div>
+                            <input class="input-paso" type="text" placeholder="Describe cómo realizar este paso">
+                        </div>
+
+
+                    </div>
+                </div>
 
 
             </div>
-            <div class="boton">
-                <button class="button" type="button" id="buscar">
-                    Añadir Receta
-                </button>
-            </div>
+
         </div>
 
         <div id="page-loader"><span class="material-icons big loading-anim"> restaurant </span></div>
