2d3accbff7b7ea7257aa573fd8a0d5d92e86c257
Nicolás de Ory <deorynicolas@gmail.com>
2020-05-29 12:20:50 +0200

    Implemented remove button

--- js/fridge.js
+++ js/fridge.js
@@ -3,30 +3,56 @@ if (!loaded)
 {
     loaded = true;
     const FRIDGE_ITEM_HTML = `
-        <div class="fridge-item">
-        <h3>{0}</h3>
-        <span class="ingred-qty">{1}</span>
-        <div class="btn-container">
-            <div class="btn"><span class="material-icons btn-icon btn-create">create</span></div>
-            <div class="btn"><span class="material-icons btn-icon btn-remove">delete</span></div>
-        </div>
+        <div class="fridge-item" ingred-id={2}>
+            <h3>{0}</h3>
+            <span class="ingred-qty">{1}</span>
+            <div class="btn-container">
+                <div class="btn editbtn"><span class="material-icons btn-icon btn-create">create</span></div>
+                <div class="btn removebtn"><span class="material-icons btn-icon btn-remove">delete</span></div>
+            </div>
         </div>
     `;
 
     const SHOPPING_ITEM_HTML = `
-        <div class="fridge-item">
-        <h3>{0}</h3>
-        <span class="ingred-qty">{1}</span>
-        <div class="btn-container">
-            <div class="btn"><span class="material-icons btn-icon btn-add">check</span></div>
-            <div class="btn"><span class="material-icons btn-icon btn-create">create</span></div>
-            <div class="btn"><span class="material-icons btn-icon btn-remove">delete</span></div>
-        </div>
+        <div class="fridge-item" ingred-id={2}>
+            <h3>{0}</h3>
+            <span class="ingred-qty">{1}</span>
+            <div class="btn-container">
+                <div class="btn addbtn"><span class="material-icons btn-icon btn-add">check</span></div>
+                <div class="btn editbtn"><span class="material-icons btn-icon btn-create">create</span></div>
+                <div class="btn removebtn"><span class="material-icons btn-icon btn-remove">delete</span></div>
+            </div>
         </div>
     `;
 
-    function getFridge()
+    function attachEventListeners() 
     {
+        // SHOPPING LIST
+        $("#lista-compra .fridge-item .removebtn").click(function()
+        {
+            const ingredID = $(this).parent().parent().attr("ingred-id");
+            $.get("delete_shopping.php?id=" + ingredID, () =>
+            {
+                getFridgeAndShoppingList();
+            });
+           
+        });
+
+        // FRIDGE
+        $("#nevera .fridge-item .removebtn").click(function()
+        {
+            const ingredID = $(this).parent().parent().attr("ingred-id");
+            $.get("delete_fridge.php?id=" + ingredID, () =>
+            {
+                getFridgeAndShoppingList();
+            });
+        });
+    }
+
+    function getFridgeAndShoppingList()
+    {
+        $("#nevera").empty();
+        $("#lista-compra").empty();
         $.get("fridge.php", function (data)
         {
             var fridge = data.nevera;
@@ -47,9 +73,7 @@ if (!loaded)
                         udMedida = "ud.";
                         break;
                 }
-                $("#nevera").append(FRIDGE_ITEM_HTML.format(itm.NOMBRE, `${itm.CANTIDAD} ${udMedida}`));
-
-                // TODO: Add click event handlers to buttons
+                $("#nevera").append(FRIDGE_ITEM_HTML.format(itm.NOMBRE, `${itm.CANTIDAD} ${udMedida}`, itm.ID_INGREDIENTE));
             });
 
             shopping.forEach((itm) =>
@@ -67,22 +91,22 @@ if (!loaded)
                         udMedida = "ud.";
                         break;
                 }
-                $("#lista-compra").append(SHOPPING_ITEM_HTML.format(itm.NOMBRE, `${itm.CANTIDAD} ${udMedida}`));
-
-                // TODO: Add click event handlers to buttons
+                $("#lista-compra").append(SHOPPING_ITEM_HTML.format(itm.NOMBRE, `${itm.CANTIDAD} ${udMedida}`, itm.ID_INGREDIENTE));
             });
+
+            attachEventListeners();
         });
     }
 
     $(document).ready(() =>
     {
 
-        getFridge();
+        getFridgeAndShoppingList();
 
     });
 }
 else
 {
-    getFridge();
+    getFridgeAndShoppingList();
 }
 
